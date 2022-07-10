524a48c2108e7e8d3b4d8ae1aae428799ad2bae5
Danillo Ilggner <danilloilggner@gmail.com>
2022-07-10 15:10:49 -0300

    little changes

--- startup.sh
+++ startup.sh
@@ -39,6 +39,9 @@ cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
 echo 'Installing FiraCode'
 sudo nala install fonts-firacode -y
 
+echo 'Installing private codecs'
+sudo nala install ubuntu-restricted-extras -y
+
 echo 'Installing NVM' 
 sh -c "$(curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash)"
 
@@ -54,10 +57,12 @@ echo 'Installing NodeJS latest version'
 nvm install node
 
 echo 'Setting NodeJS latest version as default'
-nvm alias default 18
+nvm alias default node
 
 echo 'Installing pnpm'
 npm i -g pnpm
+
+source ~/.bashrc
 
 # echo 'Installing Yarn'
 
@@ -67,78 +72,59 @@ npm i -g pnpm
 # clear
 
 echo 'Installing VSCode'
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https -y
-sudo apt-get update && sudo apt-get install code -y
+sudo nala install gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ -y
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo nala install apt-transport-https -y
+sudo nala update
+sudo nala install code -y
+
+echo 'Android Studio'
+sudo snap install android-studio --classic
+
+echo 'Installing Flutter'
+sudo snap install flutter --classic
+source ~/.bashrc
+flutter doctor -v
 
 echo 'Launching Vivaldi on Github so you can paste your keys'
 vivaldi https://github.com/settings/keys </dev/null >/dev/null 2>&1 & disown
 
 echo 'Installing Docker'
-sudo apt-get purge docker docker-engine docker.io
-sudo apt-get install docker.io -y
+sudo nala purge docker docker-engine docker.io docker-compose containerd runc -y
+sudo nala remove docker docker-engine docker.io docker-compose containerd runc -y
+sudo -- sh -c "$(curl -fsSL https://get.docker.com -o get-docker.sh | bash)"
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
-
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo chmod 777 /var/run/docker.sock
 
 echo 'Installing docker-compose'
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo nala install docker-compose
 docker-compose --version
 
-echo 'Installing Heroku CLI'
-curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-heroku --version
-
-echo 'Installing PostBird'
-wget -c https://github.com/Paxa/postbird/releases/download/0.8.4/Postbird_0.8.4_amd64.deb
-sudo dpkg -i Postbird_0.8.4_amd64.deb
-sudo apt-get install -f -y && rm Postbird_0.8.4_amd64.deb
-
-echo 'Installing Insomnia Core and Omni Theme' 
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
-  | sudo tee -a /etc/apt/sources.list.d/insomnia.list
-wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
-  | sudo apt-key add -
-sudo apt-get update && sudo apt-get install insomnia -y
-mkdir ~/.config/Insomnia/plugins && cd ~/.config/Insomnia/plugins
-git clone https://github.com/Rocketseat/insomnia-omni.git omni-theme && cd ~
-
-echo 'Installing Android Studio'
-sudo add-apt-repository ppa:maarten-fonville/android-studio -y
-sudo apt-get update && sudo apt-get install android-studio -y
+echo 'Installing Postman' 
+sudo snap install postman
 
 echo 'Installing VLC'
-sudo apt-get install vlc -y
-sudo apt-get install vlc-plugin-access-extra libbluray-bdj libdvdcss2 -y
+sudo nala install vlc -y
 
 echo 'Installing Discord'
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo dpkg -i discord.deb
-sudo apt-get install -f -y && rm discord.deb
-
-echo 'Installing Zoom'
-wget -c https://zoom.us/client/latest/zoom_amd64.deb
-sudo dpkg -i zoom_amd64.deb
-sudo apt-get install -f -y && rm zoom_amd64.deb
+sudo nala install -f -y && rm discord.deb
 
 echo 'Installing Spotify' 
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client -y
-
-echo 'Installing Peek' 
-sudo add-apt-repository ppa:peek-developers/stable -y
-sudo apt-get update && sudo apt-get install peek -y
+sudo nala update && sudo nala spotify-client -y
 
 echo 'Installing OBS Studio'
-sudo apt-get install ffmpeg && sudo snap install obs-studio
+sudo nala install ffmpeg -y && sudo snap install obs-studio
 
 echo 'Enabling KVM for Android Studio'
 sudo apt-get install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager -y
@@ -148,12 +134,8 @@ sudo adduser $USER libvirt-qemu
 echo 'Installing Robo3t'
 sudo snap install robo3t-snap
 
-echo 'Installing Lotion'
-sudo git clone https://github.com/puneetsl/lotion.git /usr/local/lotion
-cd /usr/local/lotion && sudo ./install.sh
-
 echo 'Updating and Cleaning Unnecessary Packages'
-sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
+sudo -- sh -c 'nala update; nala upgrade -y; apt full-upgrade -y; nala autoremove -y; apt autoclean -y'
 clear
 
 echo 'Installing postgis container'
