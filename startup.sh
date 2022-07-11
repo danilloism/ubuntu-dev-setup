c73c7d89f7ea0992507e32d26a6af9ddf69c91fb
Danillo Ilggner <danilloilggner@gmail.com>
2022-07-11 09:27:47 -0300

    small changes

--- startup.sh
+++ startup.sh
@@ -72,14 +72,9 @@ source ~/.bashrc
 # clear
 
 echo 'Installing VSCode'
-sudo nala install gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ -y
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo nala install apt-transport-https -y
-sudo nala update
-sudo nala install code -y
+wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+sudo dpkg -i vscode.deb
+sudo nala install -f -y && rm vscode.deb
 
 echo 'Android Studio'
 sudo snap install android-studio --classic
@@ -87,6 +82,7 @@ sudo snap install android-studio --classic
 echo 'Installing Flutter'
 sudo snap install flutter --classic
 source ~/.bashrc
+flutter doctor --android-licenses
 flutter doctor -v
 
 echo 'Launching Vivaldi on Github so you can paste your keys'
@@ -119,23 +115,20 @@ sudo dpkg -i discord.deb
 sudo nala install -f -y && rm discord.deb
 
 echo 'Installing Spotify' 
-curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo nala update && sudo nala spotify-client -y
+sudo nala update && sudo nala install spotify-client -y
 
 echo 'Installing OBS Studio'
 sudo nala install ffmpeg -y && sudo snap install obs-studio
 
 echo 'Enabling KVM for Android Studio'
-sudo apt-get install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager -y
+sudo nala install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager -y
 sudo adduser $USER libvirt
 sudo adduser $USER libvirt-qemu
 
-echo 'Installing Robo3t'
-sudo snap install robo3t-snap
-
 echo 'Updating and Cleaning Unnecessary Packages'
-sudo -- sh -c 'nala update; nala upgrade -y; apt full-upgrade -y; nala autoremove -y; apt autoclean -y'
+sudo -- sh -c 'nala update; nala upgrade -y; apt full-upgrade -y; nala autoremove -y; apt autoclean -y; nala clean; apt clean'
 clear
 
 echo 'Installing postgis container'
